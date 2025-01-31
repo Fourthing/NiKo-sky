4719e44045d0b8392ab371b335502ff643cdf7a2
Fourthing <1447444352@qq.com>
2025-01-31 19:34:21 +0800

    订单搜索 的 业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController("adminOrderController")
+@RequestMapping("/admin/order")
+@Api(tags = "订单接口")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/conditionSearch")
+    public Result<PageResult> findAll(OrdersPageQueryDTO ordersPageQueryDTO) {
+        PageResult pageResult=orderService.conditionSearch(ordersPageQueryDTO);
+        return Result.success(pageResult);
+    }
+}
